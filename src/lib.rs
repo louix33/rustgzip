db8a8104cac48cc58cb7afd9cb5e14693fa8a2b9
louis <louisliu233@163.com>
2023-07-07 18:14:05 +0800

    cli arguments parser

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,5 @@ mod deflate;
 mod gzip;
 mod error;
 mod circular_buf;
+
+pub use gzip::compress_to_gzip;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::Parser;
+
+/// A Rust implementation of GZIP compression
+#[derive(Debug, Parser)]
+struct Cli {
+    /// The input file to be compressed
+    input: String,
+    /// The output file
+    #[arg(short, long)]
+    output: Option<String>
+}
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+    rustgzip::compress_to_gzip(&args.input, &args.output.unwrap_or("a.out".to_string())).unwrap();
 }
