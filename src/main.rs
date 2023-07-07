466f756f01edcb7c8441d6ea55f43c095eb121fc
louis <louisliu233@163.com>
2023-07-07 18:21:16 +0800

    handle error in main

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use clap::Parser;
+use std::error::Error;
 
 /// A Rust implementation of GZIP compression
 #[derive(Debug, Parser)]
 struct Cli {
     /// The input file to be compressed
     input: String,
-    /// The output file
+    /// The output compressed file
     #[arg(short, long)]
     output: Option<String>
 }
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args = Cli::parse();
-    rustgzip::compress_to_gzip(&args.input, &args.output.unwrap_or("a.out".to_string())).unwrap();
+    rustgzip::compress_to_gzip(&args.input, &args.output.unwrap_or("a.out".to_string()))
 }
